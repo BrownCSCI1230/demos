858ed2b8d9ea3aec951d1cd62bd02e94a9d210f3
mbartle <mastle777@gmail.com>
2017-09-24 13:44:37 -0400

    Use PIXI to display image

--- image_processing/app.js
+++ image_processing/app.js
@@ -1,67 +1,30 @@
 $(function() {
 
-  var marquee = document.getElementById("marquee");
-  marquee.style.position = "absolute";
-  var marqueeClicked = false;
+  var mandrillCanvas = document.getElementById("mandrillCanvas");
   var image = document.getElementById("mandrill");
-  var canvas = document.getElementById("mandrillCanvas");
+  var renderer;
+  var stage;
 
-  image.onload = function() {
-      canvas.getContext("2d").drawImage(image, 0, 0, canvas.width, canvas.height);
-  }
+  setUpWidget(256, 256);
 
-  window.onload = function(){
-    setMarquee(canvas.getBoundingClientRect().left, canvas.getBoundingClientRect().top);
-    changeMarquee(256, 256);
-  }
+  function setUpWidget(width, height) {
+    renderer = PIXI.autoDetectRenderer({
+      width: width,
+      height: height,
+      view: mandrillCanvas
+    });
+    stage = new PIXI.Container;
 
-  function setMarquee(x, y) {
-    console.log(x + ", " + y);
-    marquee.style.left = x;
-    marquee.style.top = y;
-    marquee.width = 0;
-    marquee.height = 0;
-  }
-
-  function changeMarquee(x, y) {
-    if (marquee !== undefined) {
-      marquee.style.right = marquee.getBoundingClientRect().left + Math.abs(x - marquee.getBoundingClientRect().left);
-      marquee.style.bottom = marquee.getBoundingClientRect().top + Math.abs(y - marquee.getBoundingClientRect().top);
-      if (x < marquee.getBoundingClientRect().left) {
-        marquee.style.left = x;
-      }
-
-      if (y < marquee.getBoundingClientRect().top) {
-        marquee.style.top = y;
-      }
-    }
-    console.log("x: " + marquee.x + " y: " + marquee.y + " w: " + marquee.width + " h: " + marquee.height);
-  }
-
-  canvas.onmousedown = function() {
-    x = MouseEvent.x;
-    y = MouseEvent.y;
-    if (x >= canvas.getBoundingClientRect().left &&
-      x <= canvas.getBoundingClientRect().left + canvas.width &&
-      y >= canvas.getBoundingClientRect().top &&
-      y <= canvas.getBoundingClientRect().top + canvas.height) {
-      setMarquee(x, y);
-      marqueeClicked = true;
+    var mandrill = new Image()
+    mandrill.src = 'mandrill.png'
+    mandrill.onload = function(){
+      var texture = new PIXI.Texture(new PIXI.BaseTexture(mandrill));
+      var sprite = new PIXI.Sprite(texture);
+      sprite.width = renderer.width;
+      sprite.height = renderer.height;
+      stage.addChild(sprite);
+      renderer.render(stage);
     }
   }
 
-  canvas.onmousemove = function() {
-    x = MouseEvent.x;
-    y = MouseEvent.y;
-    if (x >= canvas.getBoundingClientRect().left &&
-      x <= canvas.getBoundingClientRect().left + canvas.width &&
-      y >= canvas.getBoundingClientRect().top &&
-      y <= canvas.getBoundingClientRect().top + canvas.height) {
-      changeMarquee(x, y);
-    }
-  }
-
-  canvas.onmouseup = function() {
-    marqueeClicked = false;
-  }
 })
